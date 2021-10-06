a73501bb9b1d6ef71792e4a514727658ad863d95
Marcos Colebrook <mcolesan@ull.es>
2021-10-06 09:16:47 +0100

    Updating files

--- vscode.sh
+++ vscode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #/usr/bin/code --user-data-dir $HOME/.vscode/Code --extensions-dir $HOME/.vscode/extensions --disable-gpu $@
 #/usr/bin/code --user-data-dir $HOME/.vscode/Code --disable-gpu $@
-/usr/bin/code --disable-gpu $@
+/usr/bin/code --disable-gpu . $@
