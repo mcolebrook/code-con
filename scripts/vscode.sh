f4e93807b809dd920801b823c2edd6c562f8a429
Marcos Colebrook <mcolesan@ull.es>
2021-11-12 18:34:50 +0000

    Update vscode.sh

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/dbus-launch /usr/bin/code --no-sandbox --disable-gpu --disable-gpu-sandbox --disable-software-rasterizer --wait --extensions-dir /home/user/.config/Code/extensions $@
+/usr/bin/dbus-launch /usr/bin/code --no-sandbox --disable-gpu --disable-gpu-sandbox --wait --extensions-dir /home/user/.config/Code/extensions $@
